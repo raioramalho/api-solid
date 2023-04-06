3faffa979075056abc3341d7b93994f29072054a
Alan Ramalho <ramalho.sit@gmail.com>
2023-04-05 21:46:37 -0300

    #TODO

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -4,4 +4,6 @@ import { userController } from './controllers'
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', userController.create)
   app.patch('/users/:id', userController.update)
+  // TODO: Get user by id: service and controller
+  // app.get('/users/:id', userController.select)
 }
